087c82d0cd11430312cc49b9b9297a73cd1d39e2
Sniper7Kills <willg@1337hax.org>
2019-12-22 22:59:29 -0700

    first commit

---
+++ src/SurveyServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Sniper7Kills\Survey;
+
+use Illuminate\Support\ServiceProvider;
+
+class SurveyServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+}
